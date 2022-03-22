dd065f6d068bc02befd162e4a2396ae8cec29cc7
Sejal Kothari <kotharisejal143@gmail.com>
2022-03-22 17:34:47 +0530

    otp

--- la_mita/lib/pages/otp.dart
+++ la_mita/lib/pages/otp.dart
@@ -1,12 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:la_mita/pages/widgets/themes.dart';
+import 'package:pin_input_text_field/pin_input_text_field.dart';
 
 class otp extends StatelessWidget {
   const otp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+       resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+          child: Column(
+            children: [
+              
+              SizedBox(
+                height: 18,
+              ),
+              Container(
+                width: 300,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.rectangle,
+                ),
+                child: Image.asset(
+                  'assets/images/disco.png',
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                'Verification',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Enter your OTP code number",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 1,
+              ),
+
+
+
+              Container(
+                padding: EdgeInsets.all(28),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Column(
+                  children: [
+                    
+                    SizedBox(
+                      height: 10,
+                    ),
+
+ Container(
+                child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PinInputTextField(
+             // showFieldAsBox: true,
+              onSubmit: (String pin){
+                showDialog(
+                    context: context,
+                    builder: (context){
+                      return AlertDialog(
+                        title: Text("Pin"),
+                        content: Text('Pin entered is $pin'),
+                      );
+                    }
+                ); //end showDialog()
+              }, // end onSubmit
+            ), // end PinEntryTextField()
+          ), // end Padding()
+        ),
       
+
+SizedBox(
+  height: 40,
+),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(MyTheme.orange3),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(14.0),
+                          child: Text(
+                            'Verify',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 18,
+              ),
+             
+    
+  
+
+              
+
+
+
+              Text(
+                "Didn't you receive any code?",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Text(
+                "Resend New Code",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: MyTheme.orange3,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldOTP({bool? first, last}) {
+    return Container(
+      height: 85,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: TextField(
+          autofocus: true,
+          onChanged: (value) {
+            if (value.length == 1 && last == false) {
+             // FocusScope.of(context).nextFocus();
+            }
+            if (value.length == 0 && first == false) {
+             // FocusScope.of(context).previousFocus();
+            }
+          },
+          showCursor: false,
+          readOnly: false,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          keyboardType: TextInputType.number,
+          maxLength: 1,
+          decoration: InputDecoration(
+            counter: Offstage(),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2, color: Colors.black12),
+                borderRadius: BorderRadius.circular(12)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2, color: Colors.purple),
+                borderRadius: BorderRadius.circular(12)),
+          ),
+        ),
+      ),
     );
   }
 }
+    
