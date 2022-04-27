12ec9480852e244da5ed61350be814d822840f13
jaggadas <jaggadas123@gmail.com>
2022-04-27 17:46:11 +0530

    finalised app

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -14,6 +14,7 @@ import 'package:date_field/date_field.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:la_mita/UiFake.dart' if (dart.library.html) 'dart:ui' as ui;
+import 'package:url_launcher/url_launcher.dart';
 
 
 class Payment extends StatelessWidget {
@@ -58,7 +59,27 @@ class _MypaymentformState extends State<Mypaymentform> {
   String authId=FirebaseService().getAuthId().toString();
   var amountController=TextEditingController();
   var remarksController=TextEditingController();
+  void _launchURL() async {
+    String upiId='7620780585-1@okbizaxis';
+    String merchantCode='BCR2DN6TYXE2ZMCY';
+    String name='LaMitas';
+    String amount='1';
+    String transactionId='12345678';
+    String transactionName='RentPayment';
+    //String _url='upi://pay?pa=$upiId&pn=$upiId&mc=$merchantCode&tr=$transactionId&tn=$transactionName&am=$amount&cu=INR';
+    //String _url='upi://pay?pa=7620780585-1@okbizaxis&pn=7620780585-1@okbizaxis&mid=BCR2DN6TYXE2ZMCY&mc=BCR2DN6TYXE2ZMCY&tr=12345678&tn=rent&am=1.00&cu=INR';
+    // String _url='upi://pay?pa=newdeoelectronics@indianbk&pn=newdeoelectronics@indianbk&tn=$transactionName&am=$amount&cu=INR';
+    String _url='upi://pay?pa=9334188450@paytm&pn=9334188450@paytm&mc=&tr=12345&tn=$transactionName&am=$amount&cu=INR';
+    print(_url);
+    var result = await launch(_url);
+    debugPrint(result.toString());
+    if (result ==true) {
+      print("Done");
+    } else if (result ==false){
+      print("Fail");
 
+    }
+  }
   getPlatform(){
     if(kIsWeb && (defaultTargetPlatform == TargetPlatform.android)){
       return 'Android';
@@ -212,31 +233,8 @@ class _MypaymentformState extends State<Mypaymentform> {
 
             const Text('Year', style: TextStyle(fontSize: 22)),
             RadioListTile(
-                title: const Text('2020'),
-                value: '2020',
-                groupValue: _result1,
-                onChanged: (value) {
-                  setState(() {
-                    _result1 = value;
-                    year=value.toString();
-                  });
-                }),
-
-            RadioListTile(
-                title: const Text('2021'),
-                value: '2021',
-                groupValue: _result1,
-                onChanged: (value) {
-                  setState(() {
-                    _result1 = value;
-                    year=value.toString();
-                  });
-                }),
-
-            RadioListTile(
                 title: const Text('2022'),
                 value: '2022',
-
                 groupValue: _result1,
                 onChanged: (value) {
                   setState(() {
@@ -244,6 +242,7 @@ class _MypaymentformState extends State<Mypaymentform> {
                     year=value.toString();
                   });
                 }),
+
             RadioListTile(
                 title: const Text('2023'),
                 value: '2023',
@@ -254,9 +253,31 @@ class _MypaymentformState extends State<Mypaymentform> {
                     year=value.toString();
                   });
                 }),
+
             RadioListTile(
                 title: const Text('2024'),
                 value: '2024',
+
+                groupValue: _result1,
+                onChanged: (value) {
+                  setState(() {
+                    _result1 = value;
+                    year=value.toString();
+                  });
+                }),
+            RadioListTile(
+                title: const Text('2025'),
+                value: '2025',
+                groupValue: _result1,
+                onChanged: (value) {
+                  setState(() {
+                    _result1 = value;
+                    year=value.toString();
+                  });
+                }),
+            RadioListTile(
+                title: const Text('2026'),
+                value: '2026',
                 groupValue: _result1,
                 onChanged: (value) {
                   setState(() {
@@ -292,8 +313,8 @@ class _MypaymentformState extends State<Mypaymentform> {
                   });
                 }),
             RadioListTile(
-                title: const Text('Gpay'),
-                value: 'Gpay',
+                title: const Text('UPI'),
+                value: 'UPI',
                 groupValue: _result2,
                 onChanged: (value) {
                   setState(() {
@@ -301,27 +322,8 @@ class _MypaymentformState extends State<Mypaymentform> {
                     paymentMode=value.toString();
                   });
                 }),
-            RadioListTile(
 
-                title: const Text('PhonePay'),
-                value: 'PhonePay',
-                groupValue: _result2,
-                onChanged: (value) {
-                  setState(() {
-                    _result2 = value;
-                    paymentMode=value.toString();
-                  });
-                }),
-            RadioListTile(
-                title: const Text('Paytm'),
-                value:'Paytm',
-                groupValue: _result2,
-                onChanged: (value) {
-                  setState(() {
-                    _result2 = value;
-                    paymentMode=value.toString();
-                  });
-                }),
+
                 RadioListTile(
                     title: const Text('Netbanking'),
                     value:'Netbanking',
@@ -401,7 +403,8 @@ class _MypaymentformState extends State<Mypaymentform> {
             ),
 
             SizedBox(height: 45),
-                Image.asset('assets/images/upi_qr_image.jpeg'),
+                //getPlatform()=='Desktop'||getPlatform()=='iOS'?Center(child: Image.asset('assets/images/upi_qr_image.jpeg',height: 300,)):Container(),
+                Center(child: Image.asset('assets/images/upi_qr_image.jpeg',height: 300,)),
                 // Image(image: NetworkImage('${widget.paymentDetails!.image}')),
                 SizedBox(height: 45),
             TextButton(
@@ -413,26 +416,81 @@ class _MypaymentformState extends State<Mypaymentform> {
                 foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
               ),
               onPressed: () async{
+               // if(getPlatform()=='Desktop'||getPlatform()=='iOS'){
                 var now = new DateTime.now();
                 var formatter = new DateFormat('yyyy-MM-dd');
                 String date = formatter.format(now);
-
                 if (validateForm()){
                 await FirebaseService().sendPaymentForVerification(date, paymentType, paymentMonth, year, paymentMode, paymentAmount, paymentRemarks, authId, context);
                 FlutterToastService().showToast('Payment Sent for verification');
-                print('webamoutn $webAmount');
-                print(paymentMode);
-                if(paymentMode!='Cash'&&paymentMode!='Cheque'&&paymentMode!='Security Deposit Use up'){
-                Navigator.pop(context);
-                await Navigator.push(context,MaterialPageRoute(builder: (context){
-                  return Webpayment(name: paymentType,image: '',price: webAmount,);
-                }));}
+                // if(paymentMode!='Cash'&&paymentMode!='Cheque'&&paymentMode!='Security Deposit Use up'){
+                // Navigator.pop(context);
+                // await Navigator.push(context,MaterialPageRoute(builder: (context){
+                //   return Webpayment(name: paymentType,image: '',price: webAmount,);
+                // }));}
                 resetForm();
                 }
+               // }
+                // else{
+                //   var now = new DateTime.now();
+                //   var formatter = new DateFormat('yyyy-MM-dd');
+                //   String date = formatter.format(now);
+                //   if (validateForm()){
+                //     if(paymentMode=='UPI')
+                //     _launchURL();
+                //
+                //    showDialog<void>(
+                //       context: context,
+                //       barrierDismissible: false, // user must tap button!
+                //       builder: (BuildContext context) {
+                //         return AlertDialog(
+                //           title: const Text('Payment Verification'),
+                //           content: SingleChildScrollView(
+                //             child: ListBody(
+                //               children: <Widget>[
+                //                 Text('Only press approve if you have completed the payment'),
+                //
+                //               ],
+                //             ),
+                //           ),
+                //           actions: <Widget>[
+                //             TextButton(
+                //               child:  Text('Approve',style: TextStyle(color: MyTheme.orange2)),
+                //               onPressed: () async{
+                //                 await FirebaseService().sendPaymentForVerification(date, paymentType, paymentMonth, year, paymentMode, paymentAmount, paymentRemarks, authId, context);
+                //
+                //                 FlutterToastService().showToast('Payment Sent for verification');
+                //                 resetForm();
+                //                 Navigator.of(context).pop();
+                //               },
+                //             ),
+                //             TextButton(
+                //               child:  Text('Deny',style: TextStyle(color: MyTheme.orange2),),
+                //               onPressed: () async{
+                //                 resetForm();
+                //
+                //                 Navigator.of(context).pop();
+                //               },
+                //             ),
+                //           ],
+                //         );
+                //       },
+                //     );
+                //
+                //
+                //   }
+                // }
 
               },
               child: Text('Submit'),
-            )
+            ),
+                SizedBox(height: 10,),
+                // getPlatform()=='Desktop'||getPlatform()=='iOS'?Text('Please click submit only after payment is complete',style:
+                //   TextStyle(fontSize: 15,color: Colors.grey)
+                //   ,):Container(),
+                Text('Please click submit only after payment is complete',style:
+                TextStyle(fontSize: 15,color: Colors.grey)
+                  ,)
           ]),
         ));
   }
